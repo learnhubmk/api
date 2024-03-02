635efb3220f0624cd076a93a2d3d0ad20faa9f41
nikoladomazetovikj <nikolad150@gmail.com>
2024-03-02 13:55:37 +0100

    feat: test api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Healthcheck
+ *
+ * Check that the service is up. If everything is okay, you'll get a 200 OK response.
+ *
+ * Otherwise, the request will fail with a 400 error, and a response listing the failed services.
+ *
+ * @response 400 scenario="Service is unhealthy" {"status": "down", "services": {"database": "up", "redis": "down"}}
+ * @responseField status The status of this API (`up` or `down`).
+ * @responseField services Map of each downstream service and their status (`up` or `down`).
+ */
+Route::get('/healthcheck', function () {
+    return [
+        'status' => 'up',
+        'services' => [
+            'database' => 'up',
+            'redis' => 'up',
+        ],
+    ];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
