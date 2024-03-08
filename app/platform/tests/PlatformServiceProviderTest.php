2708c65692d941307e556498832cc5665f351c05
nikoladomazetovikj <nikolad150@gmail.com>
2024-03-08 17:14:38 +0100

    installed laravel modular

---
+++ app/platform/tests/PlatformServiceProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Platform\Tests;
+
+class PlatformServiceProviderTest extends TestCase
+{
+	// TODO
+}
---
+++ app/website/src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Website\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
