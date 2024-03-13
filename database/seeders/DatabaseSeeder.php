64ae8a5d38d55d304ffb2c59df192f55eef2262e
hristinamkjiroski <mijalova@yahoo.com>
2024-03-13 23:03:55 +0100

    Set Roles and Permissions package

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,18 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user= \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $adminRole = Role::create([
+            "name" => "Admin"
+        ]);
+        Role::create([
+            "name" => "Member"
+        ]);
+        $user->assignRole($adminRole);
+
     }
 }
