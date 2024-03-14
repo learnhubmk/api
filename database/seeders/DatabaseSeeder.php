1a117ca6e38eaf7e70a8873f5ed4a9f9af4399eb
nikoladomazetovikj <nikolad150@gmail.com>
2024-03-14 18:20:32 +0100

    feat: seed initial blogs data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Website\Database\Seeders\BlogSeeder;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Artisan::call('db:seed', [
+            '--class' => BlogSeeder::class,
+            '--module' => 'Website',
+        ]);
     }
 }
