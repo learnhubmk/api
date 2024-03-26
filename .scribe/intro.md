cd77b8bdeb941c95e031df5e18f6665a922bf73a
nikoladomazetovikj <nikolad150@gmail.com>
2024-03-26 10:20:07 +0100

    feat(website-blog): scribe documentation for the list blog post api

--- .scribe/intro.md
+++ .scribe/intro.md
@@ -3,7 +3,7 @@
 
 
 <aside>
-    <strong>Base URL</strong>: <code>http://learnhub.test:80</code>
+    <strong>Base URL</strong>: <code>http://localhost:8000</code>
 </aside>
 
 This documentation aims to provide all the information you need to work with our API.
--- app/Website/src/Http/Controllers/BlogTagsController.php
+++ app/Website/src/Http/Controllers/BlogTagsController.php
@@ -7,12 +7,16 @@ use App\Website\Http\Resources\Tags\BlogPostTagResource;
 use App\Website\Models\BlogPostTag;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\QueryParam;
 
 class BlogTagsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    #[Endpoint('Website/List Blog Post Tags', <<<'DESC'
+              This endpoint list all blogpost tags in alphabetically order paginated by 20.
+              Additionally you may use ?name= query param to search by name
+    DESC)]
+    #[QueryParam('name', 'string', required: false)]
     public function index(Request $request): AnonymousResourceCollection
     {
         $tags = BlogPostTag::when($request->has('name'), function ($query) use ($request) {
