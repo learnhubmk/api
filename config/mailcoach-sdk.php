f3cd162e6104afbdd46a65e013eb83a42b52be93
hristinamkjiroski <mijalova@yahoo.com>
2024-03-27 17:38:58 +0100

    Configured the Maicoach SDK

---
+++ config/mailcoach-sdk.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /*
+     *  You'll find both the API token and endpoint on Mailcoach'
+     *  API tokens screen in the Mailcoach settings.
+     */
+    'api_token' => env('MAILCOACH_API_TOKEN'),
+
+    'endpoint' => env('MAILCOACH_API_ENDPOINT'),
+];
