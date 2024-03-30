17e660803016d3ac0e6be5260426aa627ab0aa25
Davor Minchorov <davorminchorov@gmail.com>
2024-03-30 13:57:30 +0100

    Remove TelescopeServiceProvider from the list of providers in config/app.php

--- config/app.php
+++ config/app.php
@@ -170,7 +170,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
     ])->toArray(),
 
     /*
