ab4461b2b8ff6f74097727f71797871087a8c865
Bobby Zrncev <bzrncev@users.noreply.github.com>
2024-04-02 23:22:22 +0200

    Use laravel-permission's role middleware

--- app/Admin/routes/admin-routes.php
+++ app/Admin/routes/admin-routes.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => '/admin'], function () {
     Route::post('/login', [\App\Admin\Http\Controllers\Auth\AuthController::class, 'login'])->middleware(['throttle:admin.login']);
 
-    Route::middleware(['auth:sanctum', 'verified', 'admin'])->group(function () {
+    Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->group(function () {
         Route::post('/logout', [\App\Admin\Http\Controllers\Auth\AuthController::class, 'logout']);
 
         Route::apiResource('/users', UserController::class);
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,5 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-        'admin' => AdminMiddleware::class,
     ];
 }
