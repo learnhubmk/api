236422ae9596ed1a52947ad396d50d41e0ce0fd5
Davor Minchorov <davorminchorov@gmail.com>
2024-04-17 05:13:38 +0200

    Bring back the static docs folder

--- config/scribe.php
+++ config/scribe.php
@@ -40,7 +40,7 @@ return [
     // - "laravel" will generate the documentation as a Blade view, so you can add routing and authentication.
     // - "external_static" and "external_laravel" do the same as above, but generate a basic template,
     // passing the OpenAPI spec as a URL, allowing you to easily use the docs with an external generator
-    'type' => 'laravel',
+    'type' => 'static',
 
     // See https://scribe.knuckles.wtf/laravel/reference/config#theme for supported options
     'theme' => 'elements',
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::view('/docs', 'scribe.index')->name('scribe');
