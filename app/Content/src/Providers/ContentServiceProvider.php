adbcfa14e800b391eb3901afcdd010ac78e8fb71
Davor Minchorov <davorminchorov@gmail.com>
2024-04-17 04:08:55 +0200

    Create the Content module

---
+++ app/Content/src/Providers/ContentServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Content\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContentServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
