12b31bd44b502d2dadd357c42e1649b224cd67de
Davor Minchorov <davorminchorov@gmail.com>
2024-04-17 04:19:41 +0200

    Implement the API home page JSON response

--- app/Website/routes/website-routes.php
+++ app/Website/routes/website-routes.php
@@ -3,7 +3,10 @@
 use App\Website\Http\Controllers\BlogPostController;
 use App\Website\Http\Controllers\BlogPostTagsController;
 use App\Website\Http\Controllers\ContactController;
+use App\Website\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('blog-posts', [BlogPostController::class, 'index'])->name('blog-post.index');
 Route::get('blog-posts/{slug}', [BlogPostController::class, 'show'])->name('blog-post.show');
