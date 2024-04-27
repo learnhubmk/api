3395875d62d9636102637f0084b1f8af6cb56d7e
nikoladomazetovikj <nikolad150@gmail.com>
2024-04-27 16:47:14 +0200

    feat: seed initial content user

---
+++ app/Content/database/seeders/ContentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Content\Database\Seeders;
+
+use App\Content\Models\User;
+use App\Platform\Enums\RoleName;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class ContentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'email' => 'content@learnhub.mk',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now()
+        ]);
+
+        $user->assignRole(RoleName::CONTENT->value);
+    }
+}
