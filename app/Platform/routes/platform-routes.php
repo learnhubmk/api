3eae3caaabb1aa8d37e01ff45af38cb5f379d367
nikoladomazetovikj <nikolad150@gmail.com>
2024-04-29 23:29:57 +0200

    feat: google signin routes

--- app/Platform/routes/platform-routes.php
+++ app/Platform/routes/platform-routes.php
@@ -1,1 +1,10 @@
 <?php
+
+use App\Platform\Http\Controllers\Auth\Social\AuthGoogleController;
+
+Route::group(['prefix' => '/platform'], function () {
+    Route::group(['prefix' => '/social'], function () {
+        Route::get('/google/redirect', [AuthGoogleController::class, 'redirect']);
+        Route::get('/google/callback', [AuthGoogleController::class, 'handleCallback']);
+    });
+});
