a92d7cdea26454ce85e89b4bdb425f1c46fabfbe
nikoladomazetovikj <nikolad150@gmail.com>
2024-05-03 11:44:27 +0200

    fix(content): use soft deletes for blog post tags

--- app/Website/database/migrations/2024_03_16_174322_create_tags_table.php
+++ app/Website/database/migrations/2024_03_16_174322_create_tags_table.php
@@ -13,6 +13,7 @@ return new class () extends Migration {
         Schema::create('blog_post_tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->softDeletes();
         });
     }
 
