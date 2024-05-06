97c254a3a833b5ad29536cd3bca35c453bf5b586
nikoladomazetovikj <nikolad150@gmail.com>
2024-05-06 15:29:10 +0200

    fix: typo

--- app/Content/src/Http/Requests/Auth/LogoutRequest.php
+++ app/Content/src/Http/Requests/Auth/LogoutRequest.php
@@ -12,7 +12,7 @@ class LogoutRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return $this->user()->hasRole(RoleName::CONTENT);
+        return $this->user()->hasRole(RoleName::CONTENT_MANAGER);
     }
 
     /**
