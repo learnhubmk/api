45fef092a6a541fa30a2d542e76bbc5cf7e3c1bd
y0vche <yovche@mail.com>
2024-05-17 18:45:03 +0200

    routing member sign in endpoint

--- app/Platform/routes/platform-routes.php
+++ app/Platform/routes/platform-routes.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Platform\Http\Controllers\Auth\Social\GoogleAuthController;
+use App\Content\Http\Controllers\Auth\AuthController;
 use App\Platform\Http\Controllers\Auth\Social\GitHubAuthController;
+use App\Platform\Http\Controllers\Auth\Social\GoogleAuthController;
 use App\Platform\Http\Controllers\Auth\Social\LinkedInAuthController;
 
 Route::get('/login/google/redirect', [GoogleAuthController::class, 'redirect']);
@@ -10,3 +11,13 @@ Route::get('/login/github/redirect', [GitHubAuthController::class, 'redirect']);
 Route::get('/login/github/callback', [GitHubAuthController::class, 'handleCallback']);
 Route::get('/login/linkedin/redirect', [LinkedInAuthController::class, 'redirect']);
 Route::get('/login/linkedin/callback', [LinkedInAuthController::class, 'handleCallback']);
+
+
+Route::group(['prefix' => '/member'], function () {
+  Route::post('/login', [AuthController::class, 'login'])->name('auth.login')->middleware(['throttle:login']);
+
+  Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+      Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+  });
+
+});
