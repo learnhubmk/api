fd7b78de5d02a9bd129194616e5853c59fc3d3c4
Davor Minchorov <davorminchorov@gmail.com>
2024-05-21 13:20:50 +0200

    Install and set up Treblle API analytics

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api')
-                ->middleware('api')
+                ->middleware(['api', 'treblle'])
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
