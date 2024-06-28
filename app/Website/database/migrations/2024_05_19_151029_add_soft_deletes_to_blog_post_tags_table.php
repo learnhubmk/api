079200f23ffc4521d3bbcaeff45a0ae5f82205e7
nikoladomazetovikj <nikolad150@gmail.com>
2024-06-29 01:20:06 +0200

    chore(content): add missing code parts

--- app/Website/database/migrations/2024_05_19_151029_add_soft_deletes_to_blog_post_tags_table.php
+++ app/Website/database/migrations/2024_05_19_151029_add_soft_deletes_to_blog_post_tags_table.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('blog_post_tags', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
@@ -21,7 +21,7 @@ return new class () extends Migration {
     public function down(): void
     {
         Schema::table('blog_post_tags', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 };
--- app/Website/database/migrations/2024_05_19_151324_add_soft_deletes_to_blog_posts_table.php
+++ app/Website/database/migrations/2024_05_19_151324_add_soft_deletes_to_blog_posts_table.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('blog_posts', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
@@ -21,7 +21,7 @@ return new class () extends Migration {
     public function down(): void
     {
         Schema::table('blog_posts', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 };
