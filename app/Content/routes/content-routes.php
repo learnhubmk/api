8bf3a1c4225dcef40ab7a43860d838e4e92fb764
calexandar <alexandercvet243@gmail.com>
2024-07-18 15:36:25 +0200

    changes made on routes

--- app/Content/routes/content-routes.php
+++ app/Content/routes/content-routes.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/content', 'middleware' => ['auth:sanctum', 'treblle', 'stateful', 'role:content_manager']], function () {
     Route::resource('/blog-posts', BlogPostController::class)->except('create', 'edit');
-    Route::resource('/blog-posts-tags', BlogPostTagsController::class)->only('index', 'store', 'update', 'destroy');
-    Route::patch('blog-posts/{blog_post}/statuses', [BlogPostStatusController::class, 'update']);
+    Route::resource('/blog-post-tags', BlogPostTagsController::class)->only('index', 'store', 'update', 'destroy');
+    Route::patch('blog-posts/{id}/statuses', [BlogPostStatusController::class, 'update']);
 });
