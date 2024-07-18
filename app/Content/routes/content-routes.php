584e53c16240d4a13b8c2403cf2b7a04096a3495
Davor Minchorov <davorminchorov@gmail.com>
2024-07-18 15:50:05 +0200

    Attempt to fix the name collision for the content blog post tag routes

--- app/Content/routes/content-routes.php
+++ app/Content/routes/content-routes.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/content', 'middleware' => ['auth:sanctum', 'treblle', 'stateful', 'role:content_manager']], function () {
     Route::resource('/blog-posts', BlogPostController::class)->except('create', 'edit');
-    Route::resource('/blog-post-tags', BlogPostTagsController::class)->only('index', 'store', 'update', 'destroy');
+    Route::resource('/blog-post-tags', BlogPostTagsController::class)->only('index', 'store', 'update', 'destroy')
+        ->name('index', 'content.blog-post-tags.index')
+        ->name('show', 'content.blog-post-tags.show')
+        ->name('store', 'content.blog-post-tags.store')
+        ->name('update', 'content.blog-post-tags.update')
+        ->name('destroy', 'content.blog-post-tags.destroy');
     Route::patch('blog-posts/{id}/statuses', [BlogPostStatusController::class, 'update']);
 });
