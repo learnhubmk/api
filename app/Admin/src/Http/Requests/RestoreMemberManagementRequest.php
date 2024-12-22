51a4434f4d54bd7b731d111107aa9cf1089e5891
Nikola Domazetovikj <nikolad150@gmail.com>
2024-12-22 23:48:09 +0100

    #159 - restore deleted member

---
+++ app/Admin/src/Http/Requests/RestoreMemberManagementRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Admin\Http\Requests;
+
+use App\Framework\Enums\RoleName;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RestoreMemberManagementRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->hasRole(RoleName::ADMIN);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
