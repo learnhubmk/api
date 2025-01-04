33e14d0d6826948f051b93e49550dd1317fa348d
Davor Minchorov <davorminchorov@gmail.com>
2025-01-04 17:52:15 +0100

    Udate the API docs theme

--- config/scribe.php
+++ config/scribe.php
@@ -40,10 +40,10 @@ return [
     // - "laravel" will generate the documentation as a Blade view, so you can add routing and authentication.
     // - "external_static" and "external_laravel" do the same as above, but generate a basic template,
     // passing the OpenAPI spec as a URL, allowing you to easily use the docs with an external generator
-    'type' => 'static',
+    'type' => 'external_laravel',
 
     // See https://scribe.knuckles.wtf/laravel/reference/config#theme for supported options
-    'theme' => 'elements',
+    'theme' => 'scalar',
 
     'static' => [
         // HTML documentation, assets and Postman collection will be generated to this folder.
