ac858c574910394166c50a6a245724c61348fad8
IlijaAngelov <ilija.angelov@yahoo.com>
2025-04-23 02:36:17 +0200

    rebase changes

--- config/app.php
+++ config/app.php
@@ -106,8 +106,6 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
-
     'previous_keys' => [
         ...array_filter(
             explode(',', env('APP_PREVIOUS_KEYS', ''))
